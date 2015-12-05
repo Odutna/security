37b49f5fe7b52d03affedb25b13e452a04c5bb81
aminami1127 <aminami1127@users.noreply.github.com>
2015-12-05 22:04:52 +0900

    add tcp client and server

---
+++ 2/tcp_client.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+
+def tcp():
+    # AF_INET: IPv4, SOCK_STREAM: TCP
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    client.connect((target_host, target_port))
+
+    # send receive raw byte strings
+    client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+    response = client.recv(4096)
+
+    # decode bytes to strings
+    print(response.decode())
+
+
+def udp():
+    # AF_INET: IPv4, SOCK_DGRAM: TCP
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    client.sendto(b"AAABBBCCC", (target_host, target_port))
+
+    data, addr = client.recvfrom(4096)
+
+    # decode bytes to strings
+    print(data.decode())
+
+tcp()
+udp()
