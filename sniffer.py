e6d6b565039966c65cd5a47f66d573d5cae7c905
aminami1127 <aminami1127@users.noreply.github.com>
2016-01-01 00:01:40 +0900

    add decoder

--- sniffer.py
+++ sniffer.py
@@ -2,13 +2,54 @@
 import os
 import socket
 import atexit
+import struct
+from ctypes import (
+    Structure,
+    c_ubyte,
+    c_ushort,
+    c_ulong,
+)
+
+from utils import until_interrupt
 
 MAX_SIZE = 65565
 
 
+class IP(Structure):
+    _fields_ = [
+        ("ihl",           c_ubyte, 4),
+        ("version",       c_ubyte, 4),
+        ("tos",           c_ubyte),
+        ("len",           c_ushort),
+        ("id",            c_ushort),
+        ("offset",        c_ushort),
+        ("ttl",           c_ubyte),
+        ("protocol_num",  c_ubyte),
+        ("sum",           c_ushort),
+        ("src",           c_ulong),
+        ("dst",           c_ulong)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        # human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
+
 class Sniffer(object):
     def __init__(self, host):
-        self.host = host
         # create raw socket
         self.handler = socket.socket(
             socket.AF_INET, socket.SOCK_RAW, self._choose_protocol()
@@ -40,7 +81,16 @@ class Sniffer(object):
     def recv(self):
         return self.handler.recvfrom(MAX_SIZE)
 
+    @until_interrupt
+    def decode(self):
+        raw_buffer = self.recv()[0]
+        ip_header = IP(raw_buffer[0:20])
+        print("Protocol: {} {} -> {}".format(
+            ip_header.protocol, ip_header.src_address, ip_header.dst_address
+        ))
+
+
 if __name__ == '__main__':
     sniffer = Sniffer('192.168.0.3')
     print('[*] Capture Starting...')
-    print(sniffer.recv())
+    sniffer.decode()
